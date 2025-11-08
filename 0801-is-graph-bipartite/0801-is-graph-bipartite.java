c387c5a9b75652978c4593698cdbf49eb532e1ea
Ayush-droider <pandey.ayush9605@gmail.com>
2025-11-08 21:13:57 +0530

    Time: 2 ms (34.03%), Space: 47.3 MB (6.78%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,37 +1,35 @@
+import java.util.*;
+
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
-            for (int j = 0; j < graph[i].length; j++) {
-                adj.get(i).add(graph[i][j]);
-            }
-        }
         int[] color = new int[n];
-        Arrays.fill(color, -1);
+        Arrays.fill(color, -1); 
         for (int i = 0; i < n; i++) {
             if (color[i] == -1) {
-                if (!bfsCheck(i, adj, color)) {
+                if (!bfsCheck(i, graph, color)) {
                     return false;
                 }
             }
         }
-
         return true;
     }
-    private boolean bfsCheck(int start, ArrayList<ArrayList<Integer>> adj, int[] color) {
+
+    private boolean bfsCheck(int start, int[][] graph, int[] color) {
         Queue<Integer> q = new LinkedList<>();
         q.add(start);
-        color[start] = 0;
+        color[start] = 0; 
+
         while (!q.isEmpty()) {
             int node = q.poll();
-            for (int neighbor : adj.get(node)) {
+
+            for (int neighbor : graph[node]) {
                 if (color[neighbor] == -1) {
                     color[neighbor] = 1 - color[node];
                     q.add(neighbor);
-                } else if (color[neighbor] == color[node]) {
-                    return false; 
+                } 
+                else if (color[neighbor] == color[node]) {
+                    return false;
                 }
             }
         }
