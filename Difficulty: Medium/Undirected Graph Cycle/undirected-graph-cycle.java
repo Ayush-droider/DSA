b04b897ef7038990d17edb545e3291e40b707344
Ayush-droider <pandey.ayush9605@gmail.com>
2025-11-08 15:12:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,39 +1,49 @@
 import java.util.*;
 
 class Solution {
-    public boolean isCycle(int V, int[][] edges) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < V; i++) {
-            adj.add(new ArrayList<>());
+    private class Pair {
+        int first;
+        int second;
+        Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
         }
-
-        for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            adj.get(u).add(v);
-            adj.get(v).add(u); 
-        }
-
-        boolean[] visited = new boolean[V];
-        for (int i = 0; i < V; i++) {
-            if (!visited[i]) {
-                if (dfs(i, -1, visited, adj)) {
+    }
+    public boolean bfs(int src, int[] visited, ArrayList<ArrayList<Integer>> adj) {
+        visited[src] = 1;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(src, -1));
+        while (!q.isEmpty()) {
+            int node = q.peek().first;
+            int parent = q.peek().second;
+            q.poll();
+            for (int adjNode : adj.get(node)) {
+                if (visited[adjNode] == 0) {
+                    visited[adjNode] = 1;
+                    q.add(new Pair(adjNode, node));
+                } else if (adjNode != parent) {
                     return true;
                 }
             }
         }
         return false;
     }
-
-    private boolean dfs(int node, int parent, boolean[] visited, ArrayList<ArrayList<Integer>> adj) {
-        visited[node] = true;
-        for (int neighbor : adj.get(node)) {
-            if (!visited[neighbor]) {
-                if (dfs(neighbor, node, visited, adj)) return true;
-            } else if (neighbor != parent) {
-                return true;
+    public boolean isCycle(int V, int[][] edges) {
+        int[] visited = new int[V];
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) adj.add(new ArrayList<>());
+        for (int[] e : edges) {
+            int u = e[0];
+            int v = e[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        for (int i = 0; i < V; i++) {
+            if (visited[i] == 0) {
+                if (bfs(i, visited, adj)) return true;
             }
         }
+
         return false;
     }
 }
