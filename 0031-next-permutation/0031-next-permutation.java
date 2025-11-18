54a2e8202108ac65649e01f7bca70650f1006552
Ayush-droider <pandey.ayush9605@gmail.com>
2025-11-18 14:40:35 +0530

    Time: 102 ms (1.98%), Space: 44.7 MB (10.53%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,33 +1,35 @@
 class Solution {
+    public void swap(int[] nums,int start,int end){
+        int temp=nums[start];
+        nums[start]=nums[end];
+        nums[end]=temp;
+    }
+    public void reverse(int[] nums,int start,int end){
+        while(start<end){
+            swap(nums,start,end);
+            start++;
+            end--;
+        }
+    }
     public void nextPermutation(int[] nums) {
-        helper(nums, nums.length - 2);
-    }
-
-    private void helper(int[] nums, int i) {
-        if (i < 0) {
-            reverse(nums, 0, nums.length - 1);
-            return;
+        int n=nums.length;
+        int gola_index=-1;
+        for(int i=n-1;i>0;i--){
+            if(nums[i-1]<nums[i]){
+                gola_index=i-1;
+                break;
+            }
         }
-
-        if (nums[i] < nums[i + 1]) {
-            int j = nums.length - 1;
-            while (nums[j] <= nums[i]) j--;
-            swap(nums, i, j);   
-            reverse(nums, i + 1, nums.length - 1);
-        } else {
-            helper(nums, i - 1);
+        int swap_index=gola_index;
+        if(swap_index!=-1){
+            for(int j=n-1;j>=gola_index+1;j--){
+                if(nums[gola_index]<nums[j]){
+                    swap_index=j;
+                    break;
+                }
+            }
+            swap(nums,gola_index,swap_index);
         }
-    }
-
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
-    private void reverse(int[] nums, int i, int j) {
-        if (i >= j) return;
-        swap(nums, i, j);
-        reverse(nums, i + 1, j - 1);
+        reverse(nums,gola_index+1,n-1);
     }
 }
