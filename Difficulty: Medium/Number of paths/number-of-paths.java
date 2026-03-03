e914300483334d23d3a070741e0660d909cc2912
Ayush Pandey <pandey.ayush9605@gmail.com>
2026-03-03 11:44:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of paths/number-of-paths.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numberOfPaths(int m, int n) {
+        // code here
+        int[][] dp=new int[m][n];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
+        }
+        return helper(m-1,n-1,dp);
+    }
+    public int helper(int i,int j,int[][] dp){
+        if(i==0 && j==0)return 1;
+        if(i<0 || j<0)return 0;
+        
+        if(dp[i][j]!=-1)return dp[i][j];
+        
+        int up=helper(i-1,j,dp);
+        int left=helper(i,j-1,dp);
+        
+        return dp[i][j]=up+left;
+    }
+}
