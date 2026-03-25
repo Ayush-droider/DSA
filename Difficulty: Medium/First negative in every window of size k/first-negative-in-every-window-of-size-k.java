881ead57be3568f5678d4c70e9e1adb52c604ab0
Ayush Pandey <pandey.ayush9605@gmail.com>
2026-03-25 14:40:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.java
+++ Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.java
@@ -1,32 +1,29 @@
 class Solution {
     static List<Integer> firstNegInt(int arr[], int k) {
-        int n = arr.length;
-        List<Integer> ans = new ArrayList<>();
-        Queue<Integer> q = new LinkedList<>();
-        
-        int i = 0, j = 0;
-        
-        while (j < n) {
-            if (arr[j] < 0) {
+        // write code here
+        Queue<Integer> q=new LinkedList<>();
+        List<Integer> list=new ArrayList<>();
+        int n=arr.length;
+        int i=0,j=0;
+        while(j<n){
+            if(arr[j]<0){
                 q.add(arr[j]);
             }
             
-            if (j - i + 1 == k) {
-                if (q.isEmpty()) {
-                    ans.add(0);
-                } else {
-                    ans.add(q.peek());
-                }
+            // if((j-i+1)<k)j++;
             
-                if (!q.isEmpty() && arr[i] == q.peek()) {
+            if((j-i+1)==k){
+                if(q.isEmpty())list.add(0);
+                else{
+                    list.add(q.peek());
+                }
+                if(arr[i]<0){
                     q.poll();
                 }
-                
                 i++;
             }
             j++;
         }
-        
-        return ans;
+        return list;
     }
 }
