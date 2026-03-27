bef802a5568015c1f1588f1cecf1f731e90dfd7b
Ayush Pandey <pandey.ayush9605@gmail.com>
2026-03-27 20:52:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Occurences of Anagrams/count-occurences-of-anagrams.java
+++ Difficulty: Medium/Count Occurences of Anagrams/count-occurences-of-anagrams.java
@@ -4,35 +4,33 @@ class Solution {
 
     int search(String pat, String txt) {
         // code here
+        int i=0,j=0;
         int n=txt.length();
         int k=pat.length();
         
-        int i=0,j=0;
-        int ans=0;
-        
-        HashMap<Character,Integer> map=new HashMap<>();
+        HashMap<Character,Integer> map=new HashMap<>(); 
         for(char ch:pat.toCharArray()){
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
         
         int count=map.size();
         
+        int ans=0;
+        
         while(j<n){
             char ch=txt.charAt(j);
-            
             if(map.containsKey(ch)){
                 map.put(ch,map.get(ch)-1);
                 if(map.get(ch)==0)count--;
             }
             
             if((j-i+1)==k){
-                if(count==0){
-                    ans++;
-                }
-                char ich=txt.charAt(i);
-                if(map.containsKey(ich)){
-                    map.put(ich,map.get(ich)+1);
-                    if(map.get(ich)==1)count++;
+                if(count==0)ans++;
+                
+                char leftChar=txt.charAt(i);
+                if(map.containsKey(leftChar)){
+                    map.put(leftChar,map.get(leftChar)+1);
+                    if(map.get(leftChar)==1)count++;
                 }
                 i++;
             }
