c5f61081d0bb1ef1a851dcb4c24a0b7848aff028
Ayush Pandey <pandey.ayush9605@gmail.com>
2026-04-01 17:47:46 +0530

    Time: 5 ms (66.27%), Space: 47.7 MB (6.55%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,25 +1,52 @@
 class Solution {
+    private class Steroid{
+        int size;
+        char dir;
+        int pos;
+
+        Steroid(int size,int pos,char dir){
+            this.size=size;
+            this.pos=pos;
+            this.dir=dir;
+        }
+    }
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> st = new Stack<>();
-        for (int i = 0; i < asteroids.length; i++) {
-            if (asteroids[i] > 0) {
-                st.push(asteroids[i]);
-            } else {
-                while (!st.isEmpty() && st.peek() > 0 && st.peek() < Math.abs(asteroids[i])) {
+        int n=asteroids.length;
+        Steroid[] s=new Steroid[n];
+        Stack<Steroid> st=new Stack<>();
+        for(int i=0;i<n;i++){
+            if(asteroids[i]>=0){
+                s[i]=new Steroid(asteroids[i],i,'R');
+            }
+            else s[i]=new Steroid(Math.abs(asteroids[i]),i,'L');
+        }
+        for(int i=0;i<asteroids.length;i++){
+            Steroid curr=s[i];
+
+            while(!st.isEmpty() && st.peek().dir=='R' && curr.dir=='L'){
+                Steroid top=st.peek();
+
+                if(top.size<Math.abs(curr.size)){
                     st.pop();
                 }
-                if (!st.isEmpty() && st.peek() == Math.abs(asteroids[i])) {
+                else if(top.size==Math.abs(curr.size)){
                     st.pop();
+                    curr=null;
+                    break;
                 }
-                else if (st.isEmpty() || st.peek() < 0) {
-                    st.push(asteroids[i]);
+                else{
+                    curr=null;
+                    break;
                 }
             }
+            if(curr!=null)st.push(curr);
         }
-        int[] ans = new int[st.size()];
-        for (int i = st.size() - 1; i >= 0; i--) {
-            ans[i] = st.pop();
+        int[] res = new int[st.size()];
+        for (int i = res.length - 1; i >= 0; i--) {
+            Steroid so = st.pop();
+            res[i] = so.dir == 'R' ? so.size : -so.size;
         }
-        return ans;
+
+        return res;
     }
 }
