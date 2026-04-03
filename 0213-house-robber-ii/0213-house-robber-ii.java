1f88aad8105fe04a88079ad9cf33ddca5295b5e5
Ayush Pandey <pandey.ayush9605@gmail.com>
2026-04-03 21:26:32 +0530

    Time: 0 ms (100%), Space: 43 MB (31.06%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,28 +1,36 @@
 class Solution {
+    Integer[] dp1;
+    Integer[] dp2;
     public int rob(int[] nums) {
-        int n = nums.length;
-        if(n == 1) return nums[0];   
-        int[] arr1 = new int[n-1];   
-        int[] arr2 = new int[n-1];   
-        for(int i=1;i<n;i++){
-            arr1[i-1] = nums[i];
+        int[] a=new int[nums.length-1];
+        int[] b=new int[nums.length-1];
+        if(nums.length==1)return nums[0];
+        for(int i=0;i<nums.length-1;i++){
+            a[i]=nums[i];
         }
-        for(int i=0;i<n-1;i++){
-            arr2[i] = nums[i];
-        }
-        return Math.max(helper(arr1), helper(arr2));
-    }
 
-    public int helper(int[] arr){
-        int prev = arr[0];
-        int prev2 = 0;
-        for(int i=1;i<arr.length;i++){
-            int steal = arr[i] + prev2;
-            int nosteal = prev;
-            int curr = Math.max(steal, nosteal);
-            prev2 = prev;
-            prev = curr;
+        for(int j=1;j<nums.length;j++){
+            b[j-1]=nums[j];
         }
-        return prev;
+
+        dp1=new Integer[nums.length];
+        dp2=new Integer[nums.length];
+
+        int x=helper(0,a,dp1);
+        int y=helper(0,b,dp2);
+
+        return Math.max(x,y);
+    }
+    private int helper(int idx,int[] nums,Integer[] dp){
+        if(idx>=nums.length)return 0;
+
+        if(idx==nums.length-1)return nums[idx];
+
+        if(dp[idx]!=null)return dp[idx];
+
+        int nochori=0+helper(idx+1,nums,dp);
+        int chori=nums[idx]+helper(idx+2,nums,dp);
+
+        return dp[idx]=Math.max(chori,nochori);
     }
 }
