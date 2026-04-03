aabe618d0428b310195dd6bfc8a3fc24993901cb
Ayush Pandey <pandey.ayush9605@gmail.com>
2026-04-03 21:04:11 +0530

    Time: 0 ms (100%), Space: 43 MB (27.05%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,21 +1,20 @@
 class Solution {
+    Integer[] dp;
     public int rob(int[] nums) {
-        int n=nums.length;
-        int[] dp=new int[nums.length];
-        Arrays.fill(dp,-1);
-        return Maxi(n-1,nums,dp);
+        dp=new Integer[nums.length+1];
+        return helper(0,nums,dp);
     }
-    private int Maxi(int idx,int[] arr,int[] dp){
-        if(idx==0){
-            return arr[idx];
-        }
-        if(idx<0){
-            return 0;
-        }
-        if(dp[idx]!=-1)return dp[idx];
+    private int helper(int idx,int[] nums,Integer[] dp){
 
-        int doi=arr[idx]+Maxi(idx-2,arr,dp);
-        int undo=0+Maxi(idx-1,arr,dp);
-        return dp[idx]=Math.max(doi,undo);
+        if(idx>=nums.length)return 0;
+
+        if(idx==nums.length-1)return nums[idx];
+
+        if(dp[idx]!=null)return dp[idx];
+
+        int nochori=0+helper(idx+1,nums,dp);
+        int chori=nums[idx]+helper(idx+2,nums,dp);
+
+        return dp[idx]=Math.max(chori,nochori);
     }
 }
