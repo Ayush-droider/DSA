c75faa7dbbab7532e1975603fcca3ec23443d42d
Ayush Pandey <pandey.ayush9605@gmail.com>
2026-04-20 14:28:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,49 +1,50 @@
-import java.util.*;
-
 class Solution {
-    private class Pair {
-        int first;
-        int second;
-        Pair(int first, int second) {
-            this.first = first;
-            this.second = second;
+    private class Pair{
+        int node;
+        int par;
+        
+        Pair(int node,int par){
+            this.node=node;
+            this.par=par;
         }
     }
-    public boolean bfs(int src, int[] visited, ArrayList<ArrayList<Integer>> adj) {
-        visited[src] = 1;
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(src, -1));
-        while (!q.isEmpty()) {
-            int node = q.peek().first;
-            int parent = q.peek().second;
-            q.poll();
-            for (int adjNode : adj.get(node)) {
-                if (visited[adjNode] == 0) {
-                    visited[adjNode] = 1;
-                    q.add(new Pair(adjNode, node));
-                } else if (adjNode != parent) {
-                    return true;
+    private boolean checkCycle(int node,int V,List<List<Integer>> adj,boolean[] vis){
+        vis[node]=true;
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(node,-1));
+        while(!q.isEmpty()){
+            int nod=q.peek().node;
+            int parent=q.peek().par;
+            q.remove();
+            
+            for(int neigh:adj.get(nod)){
+                if(!vis[neigh]){
+                    vis[neigh]=true;
+                    q.add(new Pair(neigh,nod));
                 }
+                else if(parent!=neigh)return true;
             }
         }
         return false;
     }
     public boolean isCycle(int V, int[][] edges) {
-        int[] visited = new int[V];
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < V; i++) adj.add(new ArrayList<>());
-        for (int[] e : edges) {
-            int u = e[0];
-            int v = e[1];
-            adj.get(u).add(v);
-            adj.get(v).add(u);
+        // Code here
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<>());
         }
-        for (int i = 0; i < V; i++) {
-            if (visited[i] == 0) {
-                if (bfs(i, visited, adj)) return true;
+        for(int[] e:edges){
+            adj.get(e[0]).add(e[1]);
+            adj.get(e[1]).add(e[0]);
+        }
+        
+        boolean[] vis=new boolean[V];
+        
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                if(checkCycle(i,V,adj,vis))return true;
             }
         }
-
         return false;
     }
 }
