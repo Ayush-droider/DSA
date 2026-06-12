72f0e3ce6119081a9786b8afab7a345acd342fb0
Ayush Pandey <pandey.ayush9605@gmail.com>
2026-06-12 21:41:04 +0530

    Time: 7 ms (36.18%), Space: 46.1 MB (59.38%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,25 +1,26 @@
 class Solution {
+    //agar map ka size tere window ki size se chhota hai matlab repeated
+    //character hai tabtak hata jab tak jyada hai agar mapsize==window size
+    //.............................FUCKED.................................
     public int lengthOfLongestSubstring(String s) {
         int i=0,j=0;
-        int n=s.length();
-        int len=0;
-        HashMap<Character,Integer> map=new HashMap<>();
-
-        while(j<n){
+        int maxi=Integer.MIN_VALUE;
+        Map<Character,Integer> map=new HashMap<>();
+        int ans=0;
+        while(j<s.length()){
             char ch=s.charAt(j);
             map.put(ch,map.getOrDefault(ch,0)+1);
 
+            if(map.size()==(j-i+1))ans=Math.max(ans,(j-i+1));
+
             while(map.size()<(j-i+1)){
-                char leftch=s.charAt(i);
-                map.put(leftch,map.get(leftch)-1);
-                if(map.get(leftch)==0)map.remove(leftch);
+                char left=s.charAt(i);
+                map.put(left,map.get(left)-1);
+                if(map.get(left)==0)map.remove(left);
                 i++;
-            }
-            if((j-i+1)==map.size()){
-                len=Math.max(len,j-i+1);
             }
             j++;
         }
-        return len;
+        return ans;
     }
 }
