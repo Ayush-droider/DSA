a24c935e495b938213afdc96cc30e028a422032a
Ayush Pandey <pandey.ayush9605@gmail.com>
2026-07-08 01:56:50 +0530

    Time: 0 ms (100%), Space: 44.8 MB (68.23%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -14,14 +14,23 @@
  * }
  */
 class Solution {
-    int sum=0;
+    private boolean isLeaf(TreeNode node){
+        return node.left==null && node.right==null;
+    }
+    private boolean helper(TreeNode root,int target,int sum){
+        if(root==null)return false;
+        sum+=root.val;
+        if(isLeaf(root)){
+            if(target==sum)return true;
+            else return false;
+        }
+        else {
+            boolean left=helper(root.left,target,sum);
+            boolean right=helper(root.right,target,sum);
+            return left||right;
+        }
+    }
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root==null){
-            return false;
-        }
-        if(root.val==targetSum && root.left==null && root.right==null){
-            return true;
-        }
-        return (hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val));
+        return helper(root,targetSum,0);
     }
 }
