e26c7f1b61b4f4d648fd25f2de973f2f4cff0ea7
Ayush Pandey <pandey.ayush9605@gmail.com>
2026-07-14 21:00:59 +0530

    Time: 53 ms (83.09%), Space: 70.8 MB (17.96%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,28 +1,23 @@
 class Solution {
-    //2- basket hai wahi k hai hamara
-    //ab kaam ye kar map le fruits collect kar per agar tujhe kisi aur type
-    //ka jyada mil rha to update maar left se hatake....FUCKED......
-    //ek aur cheej sb kuch basket cap ke ander rakhna
+    //5- min me pelunga tujhe to mai
     public int totalFruit(int[] fruits) {
-        // if(fruits.length==1)return 1;
-        int i=0,j=0;
+        if(fruits.length==1)return 1;
+        if(fruits.length==2 && fruits[0]==fruits[1])return 2;
         Map<Integer,Integer> map=new HashMap<>();
-        int ans=Integer.MIN_VALUE;
+        int ans=0;
+        int i=0,j=0;
         while(j<fruits.length){
             map.put(fruits[j],map.getOrDefault(fruits[j],0)+1);
 
-            // if(map.size()==2){
-            // }
-
             while(map.size()>2){
-                int left=fruits[i];
-                map.put(left,map.get(left)-1);
-                if(map.get(left)==0)map.remove(left);
+                int a=fruits[i];
+                map.put(a,map.get(a)-1);
+                if(map.get(a)==0)map.remove(a);
                 i++;
             }
             ans=Math.max(ans,(j-i+1));
             j++;
         }
-        return (ans==Integer.MIN_VALUE)?0:ans;
+        return ans;
     }
 }
