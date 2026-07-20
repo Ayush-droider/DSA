0e31979e63706118d9148e3f482fee66bae59f0f
Ayush Pandey <pandey.ayush9605@gmail.com>
2026-07-20 21:05:43 +0530

    Time: 2 ms (96.61%), Space: 45.7 MB (51.58%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,25 +1,22 @@
-import java.util.*;
-
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        findCombination(0, candidates, target, ans, new ArrayList<>());
-        return ans;
-    }
-
-    public void findCombination(int ind, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
-        if (ind == arr.length) {
-            if (target == 0) {
-                ans.add(new ArrayList<>(ds));
+    List<List<Integer>> ans=new ArrayList<>();
+    private void helper(int idx,int[] candidates,int target,List<Integer> list){
+        if(idx==candidates.length){
+            if(target==0){
+                ans.add(new ArrayList<>(list));
             }
             return;
         }
-
-        if (arr[ind] <= target) { 
-            ds.add(arr[ind]);
-            findCombination(ind, arr, target - arr[ind], ans, ds); 
-            ds.remove(ds.size() - 1); 
+        if(candidates[idx]<=target){
+            list.add(candidates[idx]);
+            helper(idx,candidates,target-candidates[idx],list);
+            list.remove(list.size()-1);
         }
-        findCombination(ind + 1, arr, target, ans, ds);
+        helper(idx+1,candidates,target,list);
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        helper(0,candidates,target,new ArrayList<>());
+        return ans;
     }
 }
