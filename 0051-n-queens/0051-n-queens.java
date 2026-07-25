07f32b1534e202be331432717748970eb000fbec
Ayush Pandey <pandey.ayush9605@gmail.com>
2026-07-25 15:52:57 +0530

    Time: 3 ms (39.99%), Space: 46.6 MB (65.1%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,51 +1,49 @@
 class Solution {
-    public boolean isSafe(int row,int col,List<String> board,int n){
+    List<List<String>> ans=new ArrayList<>();
+     private boolean isSafe(int row,int col,char[][] board,int n){
         int r=row;
         int c=col;
         while(r>=0 && c>=0){
-            if(board.get(r).charAt(c)=='Q') return false;
+            if(board[r][c]=='Q') return false;
             r--; c--;
         }
         r=row;
         c=col;
         while(c>=0){
-            if(board.get(r).charAt(c)=='Q') return false;
+            if(board[r][c]=='Q') return false;
             c--;
         }
         r=row;
         c=col;
         while(r<n && c>=0){
-            if(board.get(r).charAt(c)=='Q') return false;
+            if(board[r][c]=='Q') return false;
             r++; c--;
         }
         return true;
     }
-
-    public void solve(int col,List<String> board,List<List<String>> ans,int n){
+    private void solve(int col,int n,char[][] board){
         if(col==n){
-            ans.add(new ArrayList<>(board));
+            List<String> temp=new ArrayList<>();
+            for(char[] row:board){
+                temp.add(new String(row));
+            }
+            ans.add(new ArrayList<>(temp));
             return;
         }
         for(int row=0;row<n;row++){
             if(isSafe(row,col,board,n)){
-                char[] temp = board.get(row).toCharArray();
-                temp[col] = 'Q';
-                board.set(row,new String(temp));
-                solve(col+1,board,ans,n);
-                temp[col] = '.';
-                board.set(row,new String(temp));
+                board[row][col]='Q';
+                solve(col+1,n,board);
+                board[row][col]='.';
             }
         }
     }
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans=new ArrayList<>();
-        List<String> board=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            String s="";
-            for(int j=0;j<n;j++) s+=".";
-            board.add(s);
+        char[][] board=new char[n][n];
+        for(char[] rows:board){
+            Arrays.fill(rows,'.');
         }
-        solve(0,board,ans,n);
+        solve(0,n,board);
         return ans;
     }
 }
