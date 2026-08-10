34c52b7461b148e195ef3309f3fddd833e6e0d76
Ayush Pandey <pandey.ayush9605@gmail.com>
2026-08-10 21:31:44 +0530

    Time: 0 ms (100%), Space: 41.9 MB (87.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,9 @@
 class Solution {
-    Integer[] dp;
+    Integer[] dp=new Integer[46];
     public int climbStairs(int n) {
-        dp=new Integer[n+1];
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n]; 
+        if(n<0)return 0;
+        if(n==0)return 1;
+        if(dp[n]!=null)return dp[n];
+        return dp[n]=climbStairs(n-1)+climbStairs(n-2);
     }
 }
