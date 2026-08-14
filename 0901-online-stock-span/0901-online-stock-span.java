0f1f29f6aadbb6a14251750ee9d654525750346f
Ayush Pandey <pandey.ayush9605@gmail.com>
2026-08-14 21:31:51 +0530

    Time: 34 ms (21.81%), Space: 54.6 MB (30.83%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -1,16 +1,15 @@
 class StockSpanner {
-    List<Integer> arr;
+    Stack<int[]> st;
     public StockSpanner() {
-        arr=new ArrayList<>();
+        st = new Stack<>();
     }
     
     public int next(int price) {
-        arr.add(price);
-        int count=1;
-        for(int i=arr.size()-2;i>=0;i--){
-            if(arr.get(i)<=price)count++;
-            else break;
+        int count = 1;
+        while (!st.isEmpty() && st.peek()[0] <= price) {
+            count += st.pop()[1];
         }
+        st.push(new int[]{price, count});
         return count;
     }
 }
