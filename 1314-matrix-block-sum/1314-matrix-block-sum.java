142d9a2ea025ed5ea35ebec3ca1a3f956315e1c2
Ayush Pandey <pandey.ayush9605@gmail.com>
2026-08-24 01:38:32 +0530

    Time: 5 ms (98.14%), Space: 46.6 MB (80.62%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.java
+++ 1314-matrix-block-sum/1314-matrix-block-sum.java
@@ -2,17 +2,35 @@ class Solution {
     public int[][] matrixBlockSum(int[][] mat, int k) {
         int m=mat.length;
         int n=mat[0].length;
+        for(int i=0;i<m;i++){
+            int sum=0;
+            for(int j=0;j<n;j++){
+                sum+=mat[i][j];
+                mat[i][j]=sum;
+            }
+        }
+        for(int j=0;j<n;j++){
+            int sum=0;
+            for(int i=0;i<m;i++){
+                sum+=mat[i][j];
+                mat[i][j]=sum;
+            }
+        }
+
+        //prefix sum added
 
         int[][] ans=new int[m][n];
 
-        for(int i=0; i<m; i++){
-            for (int j=0; j<n;j++){
-                int sum=0;
-                for (int r=Math.max(0,i-k);r<=Math.min(m-1,i+k);r++){
-                    for(int c=Math.max(0,j-k);c<=Math.min(n-1,j+k);c++){
-                        sum+=mat[r][c];
-                    }
-                }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int x=Math.min(m-1,i+k),y=Math.min(n-1,j+k),sum=0;
+
+                sum+=mat[x][y];
+
+                if(j-k-1>-1) sum-=mat[x][j-k-1];  // left hata
+                if(i-k-1>-1) sum-=mat[i-k-1][y];  // top hata
+                if(i-k-1>-1 && j-k-1>-1) sum+=mat[i-k-1][j-k-1]; //top left do baar hata to ek baar add kar
+
                 ans[i][j]=sum;
             }
         }
