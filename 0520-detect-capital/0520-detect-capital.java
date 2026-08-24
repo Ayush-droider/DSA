bc3830901f369efa6bd2f172a397317954556b9b
Ayush Pandey <pandey.ayush9605@gmail.com>
2026-08-24 21:21:43 +0530

    Time: 1 ms (89.38%), Space: 43 MB (78.36%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int capital=0;
+        for(char ch:word.toCharArray()){
+            if(Character.isUpperCase(ch))capital++;
+        }
+        return capital==0 || (capital==1 && Character.isUpperCase(word.charAt(0))) || capital==word.length();
+    }
+}
