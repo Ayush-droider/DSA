f277bd1c827da1b5a2916112684c7639abc9aed3
Ayush Pandey <pandey.ayush9605@gmail.com>
2026-09-01 15:05:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.java
+++ Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.java
@@ -1,62 +1,54 @@
-/*
+/* Node Structure
 class Node {
     int data;
     Node left, right;
 
-    public Node(int d) {
-        data = d;
+    Node(int val) {
+        data = val;
         left = right = null;
     }
-}
-*/
+} */
 
 class Solution {
-    
-    boolean isLeaf(Node node){
-        return (node.right==null && node.left==null);
+    ArrayList<Integer> list=new ArrayList<>();
+    private boolean isLeaf(Node root){
+        return root.left==null && root.right==null;
     }
-    
-    void leftBoundary(Node node, ArrayList<Integer> list){
-        while(node != null){
-            if(!isLeaf(node)){
-                list.add(node.data);
-            }
-
-            if(node.left != null)
-                node = node.left;
-            else
-                node = node.right;
+    private void addLeft(Node root){
+        while(root!=null){
+            if(!isLeaf(root))list.add(root.data);
+            if(root.left!=null)root=root.left;
+            else root=root.right;
         }
     }
-    
-    void rightBoundary(Node node,ArrayList<Integer> list){
+    private void addRight(Node root){
         Stack<Integer> st=new Stack<>();
-        while(node!=null){
-            if(!isLeaf(node))st.add(node.data);
-            if(node.right!=null)node=node.right;
-            else node=node.left;
+        while(root!=null){
+            if(!isLeaf(root))st.push(root.data);
+            if(root.right!=null)root=root.right;
+            else root=root.left;
         }
         while(!st.isEmpty()){
             list.add(st.pop());
         }
     }
-    
-    void addLeaves(Node node,ArrayList<Integer> list){
-        if(node==null)return;
-        if(isLeaf(node))list.add(node.data);
-        addLeaves(node.left,list);
-        addLeaves(node.right,list);
+    private void addLeaves(Node root){
+        if(root==null)return;
+        if(isLeaf(root)){
+            list.add(root.data);
+            return;
+        }
+        addLeaves(root.left);
+        addLeaves(root.right);
     }
-    
-    ArrayList<Integer> boundaryTraversal(Node root) {
+    public ArrayList<Integer> boundaryTraversal(Node root) {
         // code here
-        ArrayList<Integer> list=new ArrayList<>();
         if(root==null)return list;
-        if(!isLeaf(root))list.add(root.data);//adding root
         
-        leftBoundary(root.left,list);//left Boundary
-        addLeaves(root,list);//leaves
-        rightBoundary(root.right,list);//right Boundary
+        if(!isLeaf(root))list.add(root.data);
+        addLeft(root.left);
+        addLeaves(root);
+        addRight(root.right);
         
         return list;
     }
