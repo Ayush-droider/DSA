841889c96145873b6ad84dea7e7fcd1a11d0cb65
Ayush Pandey <pandey.ayush9605@gmail.com>
2026-09-08 00:08:08 +0530

    Time: 1 ms (13.84%), Space: 45 MB (12.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,14 +1,17 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
+        Integer[][] dp=new Integer[101][101];
+        return helper(0,0,m,n,dp);
+    }
+    private int helper(int i,int j,int m,int n,Integer[][] dp){
+        if(i>=m || i<0 || j<0 || j>=n)return 0;
+        if(i==m-1 && j==n-1)return 1;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 || j == 0) dp[i][j] = 1;  
-                else dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-            }
-        }
+        if(dp[i][j]!=null)return dp[i][j];
 
-        return dp[m - 1][n - 1];
+        int down=helper(i+1,j,m,n,dp);
+        int right=helper(i,j+1,m,n,dp);
+
+        return dp[i][j]=right+down;
     }
 }
