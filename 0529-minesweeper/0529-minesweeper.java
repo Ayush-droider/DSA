5fad2397b4f1f5a902160ef6732e9e12df09bed2
Ayush Pandey <pandey.ayush9605@gmail.com>
2026-09-15 11:05:19 +0530

    Time: 4 ms (14.19%), Space: 47 MB (34.93%) - LeetHub

---
+++ 0529-minesweeper/0529-minesweeper.java
@@ -0,0 +1,73 @@
+class Solution {
+    private class Pair{
+        int i;
+        int j;
+        Pair(int i,int j){
+            this.i=i;
+            this.j=j;
+        }
+    }
+    int[] dr={0,0,1,-1,1,-1,-1,1};
+    int[] dc={1,-1,0,0,1,1,-1,-1};
+
+    private int haveAdjMines(int i,int j,char[][] board){
+        int count=0; 
+
+        for(int k=0;k<8;k++){
+            int row=i+dr[k];
+            int col=j+dc[k];
+
+            if(row>=0 && col>=0 && row<board.length && col<board[0].length){
+                if(board[row][col]=='M')count++;
+            }
+        }
+        return count;
+    }
+
+    private void bfs(int i,int j,boolean[][] vis,char[][] board){
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(i,j));
+        vis[i][j]=true;
+        while(!q.isEmpty()){
+            Pair p=q.poll();
+            int r=p.i;
+            int c=p.j;
+
+            if(board[r][c]=='M'){
+                board[r][c]='X';
+                break;
+            }
+
+            int cnt=haveAdjMines(r,c,board);
+
+            if(board[r][c]=='E' && cnt!=0){
+                board[r][c]=(char)('0'+cnt);
+            }
+            else if(board[r][c]=='E' && cnt==0){
+                board[r][c]='B';
+
+                for(int k=0;k<8;k++){
+                    int row=r+dr[k];
+                    int col=c+dc[k];
+
+                    if(row>=0 && col>=0 && row<board.length && col<board[0].length && !vis[row][col] && board[row][col]=='E'){
+                        vis[row][col]=true;
+                        q.add(new Pair(row,col));
+                    }
+                }
+            }
+        }
+    }
+
+    public char[][] updateBoard(char[][] board,int[] click){
+        boolean[][] vis=new boolean[board.length][board[0].length];
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(i==click[0] && j==click[1]){
+                    bfs(i,j,vis,board);
+                }
+            }
+        }
+        return board;
+    }
+}
